e1e1adfb4ce68bd02698c0f4c846da039dd122ab
Jujube2005 <chotika.jakchai@gmail.com>
2025-09-10 09:46:13 +0700

    Create Loan.ts

---
+++ Loan.ts
@@ -0,0 +1,47 @@
+import { LibraryItem } from "./LibraryItem";
+import { LibraryMember } from "./LibraryMember";
+import { Payment, PaymentMethod } from "./Payment";
+
+export class Loan {
+  returnDate?: Date;
+  fine: number = 0;
+  payment?: Payment;
+
+  constructor(
+    public loanId: string,
+    public item: LibraryItem,
+    public member: LibraryMember,
+    public loanDate: Date,
+    public dueDate: Date
+  ) {}
+
+  calculateFine(): number {
+    if (!this.returnDate) return 0;
+    const daysLate = Math.max(
+      0,
+      Math.floor((this.returnDate.getTime() - this.dueDate.getTime()) / (1000 * 60 * 60 * 24))
+    );
+    this.fine = daysLate * 10; // 10 per day
+    return this.fine;
+  }
+
+  closeLoan(returnDate: Date): void {
+    this.returnDate = returnDate;
+    this.calculateFine();
+  }
+
+  payFine(method: PaymentMethod): Payment | undefined {
+    if (this.fine > 0) {
+      const payment = new Payment(
+        "P-" + Date.now(),
+        this.member,
+        this.fine,
+        method
+      );
+      this.payment = payment;
+      this.fine = 0;
+      return payment;
+    }
+    return undefined;
+  }
+}
