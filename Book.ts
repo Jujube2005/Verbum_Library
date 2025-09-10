c9049a34cefcbf1b80de307b458af191703eb6c8
Jujube2005 <chotika.jakchai@gmail.com>
2025-09-10 09:42:33 +0700

    Delete Book.ts

--- Book.ts
+++
@@ -1,9 +0,0 @@
-export class Book extends LibraryItem {
-  constructor(
-    title: string,
-    itemId: string,
-    private author: string,
-    private pages: number
-  ) {
-    super(title, itemId);
-  }
